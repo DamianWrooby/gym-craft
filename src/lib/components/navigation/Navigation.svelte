21834258aaa2a8159a48619c87682fa7e619a81a
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-05-30 23:30:40 +0200

    added user data loading and navigation component

---
+++ src/lib/components/navigation/Navigation.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+
+    const user = $page.data.user;
+    let isActive = {
+        login: false,
+        register: false,
+        admin: false,
+    };
+</script>
+
+<nav class="flex text-surface-500 font-semibold">
+    {#if !user}
+        <a
+            class={isActive.login ? 'px-4 text-tertiary-500' : 'px-4'}
+            on:mouseover={() => (isActive.login = true)}
+            on:mouseout={() => (isActive.login = false)}
+            href="/login">Login</a
+        >
+        <a
+            class={isActive.register ? 'px-4 text-tertiary-500' : 'px-4'}
+            on:mouseover={() => (isActive.register = true)}
+            on:mouseout={() => (isActive.register = false)}
+            href="/register">Register</a
+        >
+    {:else}
+        <a
+            class={isActive.admin ? 'px-4 text-tertiary-500' : 'px-4'}
+            on:mouseover={() => (isActive.admin = true)}
+            on:mouseout={() => (isActive.admin = false)}
+            href="/admin">Admin</a
+        >
+
+        <form action="/logout" method="POST">
+            <button type="submit">Log out</button>
+        </form>
+    {/if}
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,35 @@
-<script lang='ts'>
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
+<script lang="ts">
+    // The ordering of these imports is critical to your app working properly
+    import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+    // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+    // Most of your app wide CSS should be put in this file
+    import '../app.postcss';
+    import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+    import { appConfig } from '../constants/app.constants';
+    import Navigation from '@components/navigation/Navigation.svelte';
 </script>
 
-<slot />
+<svelte:head>
+    <title>Gym Craft - Personal trainer powered by AI</title>
+</svelte:head>
+
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar background="bg-primary-500">
+            <svelte:fragment slot="lead">{appConfig.name}</svelte:fragment>
+            <svelte:fragment slot="trail">
+                <Navigation />
+                <LightSwitch />
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+    <!-- Router Slot -->
+    <main>
+        <slot />
+    </main>
+    <!-- ---- / ---- -->
+    <svelte:fragment slot="footer">
+        <div class="bg-primary-500">FOOTER</div>
+    </svelte:fragment>
+</AppShell>
