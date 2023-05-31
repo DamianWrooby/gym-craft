cb8371cecc3c5d6fe176d083d7be2063cece452f
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-05-31 23:14:24 +0200

    fixed navigation hover + added hero section to the main page

--- src/lib/components/navigation/Navigation.svelte
+++ src/lib/components/navigation/Navigation.svelte
@@ -2,34 +2,14 @@
     import { page } from '$app/stores';
 
     const user = $page.data.user;
-    let isActive = {
-        login: false,
-        register: false,
-        admin: false,
-    };
 </script>
 
 <nav class="flex text-surface-500 font-semibold">
     {#if !user}
-        <a
-            class={isActive.login ? 'px-4 text-tertiary-500' : 'px-4'}
-            on:mouseover={() => (isActive.login = true)}
-            on:mouseout={() => (isActive.login = false)}
-            href="/login">Login</a
-        >
-        <a
-            class={isActive.register ? 'px-4 text-tertiary-500' : 'px-4'}
-            on:mouseover={() => (isActive.register = true)}
-            on:mouseout={() => (isActive.register = false)}
-            href="/register">Register</a
-        >
+        <a class="px-4 hover:text-tertiary-500" href="/login">Login</a>
+        <a class="px-4 hover:text-tertiary-500" href="/register">Register</a>
     {:else}
-        <a
-            class={isActive.admin ? 'px-4 text-tertiary-500' : 'px-4'}
-            on:mouseover={() => (isActive.admin = true)}
-            on:mouseout={() => (isActive.admin = false)}
-            href="/admin">Admin</a
-        >
+        <a class="px-4 hover:text-tertiary-500" href="/admin">Admin</a>
 
         <form action="/logout" method="POST">
             <button type="submit">Log out</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-row">
+    <div class="basis-1/2">
+        <section class="p-5 pt-20 flex flex-col justify-items-center">
+            <h1 class="h1 text-5xl text-center">
+                <span
+                    class="bg-gradient-to-br from-primary-500 to-surface-500 bg-clip-text text-transparent box-decoration-clone font-extrabold">
+                    GYM CRAFT
+                </span>
+            </h1>
+            <p
+                class="text-center top-15 bg-gradient-to-br from-primary-500 to-primary-300 bg-clip-text text-transparent box-decoration-clone font-bold text-lg">
+                Your AI powered personal trainer
+            </p>
+        </section>
+    </div>
+    <div class="basis-1/2" />
+</div>
