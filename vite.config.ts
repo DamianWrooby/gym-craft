1c3cc3a6c886249c51bcdab63710d6c5a9585e76
Damian WROBLEWSKI <damian.wroblewski.external@atos.net>
2023-06-01 21:45:38 +0200

    fixed aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/lib/components'),
+        },
+    },
+    plugins: [sveltekit()],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}'],
+    },
 });
