afc80ed9e004393de0ff1cb15fd3fadabd843b43
WROBLEWSKI Damian <damian.wroblewski.external@atos.net>
2023-12-28 21:00:31 +0100

    skeleton v2 migration + purgecss

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: vitePreprocess(),
+    preprocess: [vitePreprocess({})],
 
     vitePlugin: {
         inspector: true,
