a9bc501417a77cc1d434fe909b83cc2823a4786f
WROBLEWSKI Damian <damian.wroblewski.external@atos.net>
2023-12-30 22:02:44 +0100

    feat(survey) - added safety info modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
-    // The ordering of these imports is critical to your app working properly
-    import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-    // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-    import '@skeletonlabs/skeleton/styles/skeleton.css';
-    // Most of your app wide CSS should be put in this file
-    import '../app.postcss';
+    import '../app.pcss';
     import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+    import { initializeStores } from '@skeletonlabs/skeleton';
+    import { Modal } from '@skeletonlabs/skeleton';
     import { HomeIcon } from 'svelte-feather-icons';
     import Navigation from '@components/navigation/Navigation.svelte';
 
     const currentDate = new Date();
+    initializeStores();
 </script>
 
 <svelte:head>
     <title>Gym Craft - Personal trainer powered by AI</title>
 </svelte:head>
 
+<Modal />
+
 <AppShell>
     <svelte:fragment slot="header">
         <AppBar background="bg-primary-500">
             <svelte:fragment slot="lead">
-                <a class="px-4 text-surface-500 hover:text-tertiary-500" href="/"><HomeIcon /></a>
+                <a class="px-4 text-surface-500 hover:text-tertiary-500" href="/"
+                    ><HomeIcon></HomeIcon></a>
             </svelte:fragment>
             <svelte:fragment slot="trail">
-                <Navigation />
-                <LightSwitch />
+                <Navigation></Navigation>
+                <LightSwitch></LightSwitch>
             </svelte:fragment>
         </AppBar>
     </svelte:fragment>
