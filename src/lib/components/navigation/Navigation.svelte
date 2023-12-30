d4eab62f39654932e4dd0a3cfd8a6648dc7c3ebb
WROBLEWSKI Damian <damian.wroblewski.external@atos.net>
2023-12-30 22:00:09 +0100

    feat(navigation) - added my account avatar

--- src/lib/components/navigation/Navigation.svelte
+++ src/lib/components/navigation/Navigation.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
     import { page } from '$app/stores';
+    import { Avatar } from '@skeletonlabs/skeleton';
 
     const user = $page.data.user;
+    const firstLetter = user.name[0].toUpperCase();
 </script>
 
-<nav class="flex text-surface-500 font-semibold whitespace-nowrap">
+<nav class="flex items-center text-surface-500 font-semibold whitespace-nowrap">
     {#if !user}
         <a class="px-4 hover:text-tertiary-500" href="/login">Login</a>
         <a class="px-4 hover:text-tertiary-500" href="/register">Register</a>
     {:else}
-        <span class="hidden md:block font-normal text-tertiary-500">Logged in as {user.name}</span>
+        <a class="px-4 hover:text-tertiary-500" href="/my-account">
+            <Avatar
+                initials={firstLetter}
+                background="bg-tertiary-900"
+                border="border-2 border-surface-300-600-token hover:!border-tertiary-500"
+                width="w-8" />
+        </a>
         <a class="px-4 hover:text-tertiary-500" href="/my-plans">My plans</a>
 
         <form action="/logout" method="POST">
