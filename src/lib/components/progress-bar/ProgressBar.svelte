47104f7655e810d05b6c259be0b6d05570d4d5b4
WROBLEWSKI Damian <damian.wroblewski.external@atos.net>
2023-12-30 22:01:40 +0100

    feat(survey) - added progress bar

---
+++ src/lib/components/progress-bar/ProgressBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let max: number;
+    export let curr: number;
+    export let duration: number;
+</script>
+
+<div class="bg-primary-900 w-full h-2 absolute left-0 top-0">
+    <div
+        class="bg-primary-500 h-full"
+        style:width="{(curr / max) * 100}%"
+        style:transition="width {duration}ms ease" />
+</div>
