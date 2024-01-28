fbae04ef7734e2a566669e2f80119ed94616d485
WROBLEWSKI Damian <damian.wroblewski.external@atos.net>
2024-01-28 19:54:40 +0100

    feat(api-plan): include generated plans number in the response object

--- src/routes/api/plan/+server.ts
+++ src/routes/api/plan/+server.ts
@@ -1,15 +1,15 @@
 import { createCompletion } from '$lib/server/openai';
 import type { ChatMessage } from '@/models/open-ai/chat-gpt.model';
 import { appConfig } from '@/constants/app.constants';
-import { incrementUserGeneretedPlans, addPlan } from '$lib/prisma/prisma';
+import { addPlan, updateGeneratedPlansNumber } from '$lib/prisma/prisma';
 
-export async function POST({ request }: { request: Request }) {
+export async function POST({ request }: { request: Request }): Promise<Response> {
     const body = await request.json();
     const messages: Array<ChatMessage> = body.messages;
-    const generatedPlans: number = body.user.generatedPlans;
+    const generatedPlansNumber: number = body.user.generatedPlansNumber;
     const userId = body.user.id;
 
-    if (generatedPlans > appConfig.planLimit) {
+    if (generatedPlansNumber > appConfig.planLimit) {
         // TODO: add error handling
         return new Response(JSON.stringify({ error: 'No plans left' }), { status: 400 });
     }
@@ -17,7 +17,7 @@ export async function POST({ request }: { request: Request }) {
     const planDescription = await createCompletion(messages);
 
     const plan = {
-        name: 'Plan ' + generatedPlans,
+        name: 'Plan ' + generatedPlansNumber,
         description: planDescription,
         User: {
             connect: {
@@ -27,7 +27,14 @@ export async function POST({ request }: { request: Request }) {
     };
 
     const generatedPlan = await addPlan(plan);
-    await incrementUserGeneretedPlans(userId);
+    const newGeneratedPlansNumber = await updateGeneratedPlansNumber(userId);
 
-    return new Response(JSON.stringify(generatedPlan));
+    const responseBody = {
+        generatedPlan: generatedPlan,
+        generatedPlansNumber: newGeneratedPlansNumber,
+    };
+
+    return new Response(JSON.stringify(responseBody), {
+        headers: { 'Content-Type': 'application/json' },
+    });
 }
