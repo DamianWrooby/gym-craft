a16c7585a47c363da5815b87b7e7e83300bc67fe
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-03-18 21:23:44 +0100

    feat(layout): added card component

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,6 +2,7 @@
     import { enhance, applyAction } from '$app/forms';
     import { makeToast } from '$lib/utils/toasts.js';
     import { getToastStore } from '@skeletonlabs/skeleton';
+    import Card from '@components/card/Card.svelte';
 
     export let form;
 
@@ -21,7 +22,6 @@
                 setTimeout(() => {
                     makeToast(toastStore, 'You are logged in.', 'variant-filled-success');
                 }, 2000);
-                
             } else if (result.type === 'error') {
                 setTimeout(() => {
                     makeToast(toastStore, result.message, 'variant-filled-error');
@@ -31,43 +31,38 @@
     };
 </script>
 
-<div class="h-full flex items-center justify-center">
-    <div class="card p-16 min-w-[30%]">
-        <section class="grow flex flex-col justify-center">
-            <h1 class="h1 text-2xl text-center font-bold">
-                <span>Hello!</span>
-            </h1>
-            <p class="pb-8 text-center">Sign in to your account</p>
-            <form action="?/login" method="POST"
-                    use:enhance={formCallback}>
-                <div class="p-1">
-                    <label class="label" for="username">Username</label>
-                    <input class="input" id="username" name="username" type="text" required />
-                </div>
+<Card width="30">
+    <section class="grow flex flex-col justify-center">
+        <h1 class="h1 text-2xl text-center font-bold">
+            <span>Hello!</span>
+        </h1>
+        <p class="pb-8 text-center">Sign in to your account</p>
+        <form action="?/login" method="POST" use:enhance={formCallback}>
+            <div class="p-1">
+                <label class="label" for="username">Username</label>
+                <input class="input" id="username" name="username" type="text" required />
+            </div>
 
-                <div class="p-1 pb-5">
-                    <label class="label" for="password">Password</label>
-                    <input class="input" id="password" name="password" type="password" required />
-                </div>
+            <div class="p-1 pb-5">
+                <label class="label" for="password">Password</label>
+                <input class="input" id="password" name="password" type="password" required />
+            </div>
 
-                {#if form?.invalid}
-                    <p class="text-primary-500 pb-5">Username and password are required.</p>
-                {/if}
+            {#if form?.invalid}
+                <p class="text-primary-500 pb-5">Username and password are required.</p>
+            {/if}
 
-                {#if form?.credentials}
-                    <p class="text-primary-500 pb-5">You have entered the wrong credentials.</p>
-                {/if}
+            {#if form?.credentials}
+                <p class="text-primary-500 pb-5">You have entered the wrong credentials.</p>
+            {/if}
 
-                <div class="text-center">
-                    <button class="btn variant-filled-primary" type="submit" disabled={loading}>Log in</button>
-                </div>
+            <div class="text-center">
+                <button class="btn variant-filled-primary" type="submit" disabled={loading}>Log in</button>
+            </div>
 
-                <p class="pt-5 text-center">
-                    Don't have an account? <a
-                        class="text-primary-500 hover:text-primary-700"
-                        href="/register">Register.</a>
-                </p>
-            </form>
-        </section>
-    </div>
-</div>
+            <p class="pt-5 text-center">
+                Don't have an account? <a class="text-primary-500 hover:text-primary-700" href="/register">Register.</a>
+            </p>
+        </form>
+    </section>
+</Card>
