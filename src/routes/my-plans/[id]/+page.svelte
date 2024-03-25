bb479cc2a291185410472c8b6595cfdb65bc3585
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-03-25 22:30:49 +0100

    feat(generated-plan-page): added generated plan page

---
+++ src/routes/my-plans/[id]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page, navigating } from '$app/stores';
+    import { goto } from '$app/navigation';
+    import { ArrowLeftIcon } from 'svelte-feather-icons';
+    import Card from '$lib/components/card/Card.svelte';
+    import Spinner from '$lib/components/loading/spinner/Spinner.svelte';
+    import type { Plan } from '@prisma/client';
+
+    const plan: Plan | null = $page.data.plan;
+
+    function goBackToPlanList() {
+        goto('/my-plans');
+    }
+</script>
+
+<Card width="75">
+    {#if $navigating}
+        <Spinner size={10} />
+    {:else}
+        <button type="button" on:click={() => goBackToPlanList()}>
+            <ArrowLeftIcon class="cursor-pointer text-surface-400 hover:text-surface-300" />
+        </button>
+        {#if plan !== null}
+            <h2 class="h2 text-center text-xl py-10">{plan.name}</h2>
+            {@html plan.description}
+        {:else}
+            <h2 class="h2 text-center text-xl py-10">Plan not found</h2>
+        {/if}
+    {/if}
+</Card>
