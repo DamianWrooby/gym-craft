5059ef565b36cb6390dfd43d334d08fe9ebe8e11
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-03-27 18:55:24 +0100

    feat(api-user): improve response + removed comment

--- src/routes/api/user/[id]/plans/+server.ts
+++ src/routes/api/user/[id]/plans/+server.ts
@@ -3,7 +3,6 @@ import { createErrorResponse } from '$lib/utils/error-response';
 import { json } from '@sveltejs/kit';
 
 export async function GET({ params }: { params: { userId: string } }): Promise<Response> {
-	// TODO: add authentication
     const userId = params.userId;
 
     const plans = await getPlans(userId);
