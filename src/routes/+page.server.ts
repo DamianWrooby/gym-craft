24e675d49482fb49b2e738b50d578fd2ee314d1f
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-04-21 12:48:37 +0200

    feat(configuration): added manual env logging

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
+import { APP_ENV } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
     // console.log(event);
-    console.log(`context: ${process.env.CONTEXT}`);
+    console.log(`env: ${APP_ENV}`);
 };
