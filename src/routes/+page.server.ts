528800298092b11eac91c04802d1fe771f499379
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-04-21 12:36:48 +0200

    feat(configuration): added context logging

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
     // console.log(event);
-    console.log(`env: ${process.env.NODE_ENV}`);
+    console.log(`context: ${process.env.CONTEXT}`);
 };
