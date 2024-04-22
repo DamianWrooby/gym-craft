446284d0198162067ffe4ac9ec95f0a2a3ba0ba6
WROBLEWSKI Damian <damian.wroblewski@eviden.com>
2024-04-22 19:01:02 +0200

    feat(error-handling): added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/stores';
+    import Card from '$lib/components/card/Card.svelte';
+</script>
+
+<Card>
+    <h1 class="h1 text-center text-5xl px-4">{$page.status}</h1>
+    <p class="text-center text-primary-500 pb-8">{$page.error?.message}</p>
+    <div class="text-center">
+        <a class="text-secondary-400 hover:text-secondary-300" href="/">Home Page</a>
+    </div>
+</Card>
