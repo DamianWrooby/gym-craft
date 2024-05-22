ecf2e41dd0433ab2773b12542fcb74b66a3119d4
Damian Wróblewski <dwroblewski89@gmail.com>
2024-05-22 19:19:28 +0200

    feat(configuration): added prod proxy url

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -2,7 +2,7 @@ export enum appConfig {
     name = 'Gym Craft',
     openAIapiUrl = 'https://api.openai.com/v1/chat/completions',
     proxyApiUrlDEV = 'http://localhost:3000/api/generate-plan',
-    proxyApiUrlPROD = '',
+    proxyApiUrlPROD = 'https://gym-craft-ai-proxy.onrender.com/api/generate-plan',
     plansApiUrl = '/api/plans',
     model = 'gpt-3.5-turbo',
     openAIcompletionSeed = 1,
