adc9e4b0120f6319234bc1d47fab3b990d2a29d7
Damian Wróblewski <dwroblewski89@gmail.com>
2024-07-08 19:23:55 +0200

    feat(my-plans): added save as pdf functionality - closes #107

--- src/routes/my-plans/[id]/+page.server.ts
+++ src/routes/my-plans/[id]/+page.server.ts
@@ -1,5 +1,8 @@
 import { getPlan } from '$lib/prisma/prisma';
 
+export const csr = true;
+export const ssr = false;
+
 export async function load({ locals, params }) {
 	const userId = locals?.user?.id;
 	return {
--- src/routes/my-plans/[id]/+page.svelte
+++ src/routes/my-plans/[id]/+page.svelte
@@ -4,9 +4,11 @@
     import { ArrowLeftIcon } from 'svelte-feather-icons';
     import Card from '$lib/components/card/Card.svelte';
     import Spinner from '$lib/components/loading/spinner/Spinner.svelte';
+    import DownloadAsPdf from '$lib/components/download-as-pdf/DownloadAsPdf.svelte';
     import type { Plan } from '@prisma/client';
 
     const plan: Plan | null = $page.data.plan;
+    let planContainer: HTMLElement | null = null;
 
     function goBackToPlanList() {
         goto('/my-plans');
@@ -17,12 +19,19 @@
     {#if $navigating}
         <Spinner size={10} />
     {:else}
-        <button type="button" on:click={() => goBackToPlanList()}>
-            <ArrowLeftIcon class="cursor-pointer text-surface-400 hover:text-surface-300" />
-        </button>
+        <div class="flex justify-between">
+            <button type="button" on:click={() => goBackToPlanList()}>
+                <ArrowLeftIcon class="cursor-pointer text-surface-400 hover:text-surface-300" />
+            </button>
+            {#if plan && planContainer}
+                <DownloadAsPdf htmlElement={planContainer} fileName={plan.name} />
+            {/if}
+        </div>
         {#if plan !== null}
-            <h2 class="h2 text-center text-xl py-10">{plan.name}</h2>
-            {@html plan.description}
+            <div class="mb-5" bind:this={planContainer}>
+                <h2 class="h2 text-center text-xl py-10">{plan.name}</h2>
+                {@html plan.description}
+            </div>
         {:else}
             <h2 class="h2 text-center text-xl py-10">Plan not found</h2>
         {/if}
