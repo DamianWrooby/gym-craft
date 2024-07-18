74843ce42c0351117a1555915a9b2615fa240aa4
Damian Wróblewski <dwroblewski89@gmail.com>
2024-07-18 22:38:44 +0200

    feat(database): added db query time logging

--- src/routes/my-plans/+page.server.ts
+++ src/routes/my-plans/+page.server.ts
@@ -2,7 +2,10 @@ import { getPlans } from '$lib/prisma/prisma';
 
 export async function load({ locals }) {
 	const userId = locals?.user?.id;
+	console.time('getPlans');
+	const plans = await getPlans(userId);
+	console.timeEnd('getPlans');
 	return {
-		plans: await getPlans(userId),
+		plans,
 	};
 }
