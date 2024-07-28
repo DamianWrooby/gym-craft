fde3df73f46f1ff136980b5263e80ec5814fb761
Damian Wróblewski <dwroblewski89@gmail.com>
2024-07-28 10:40:01 +0200

    fix(my-plans): set page as prerendered

--- src/routes/my-plans/+page.server.ts
+++ src/routes/my-plans/+page.server.ts
@@ -1,11 +1,1 @@
-import { getPlans } from '$lib/prisma/prisma';
-
-export async function load({ locals }) {
-	const userId = locals?.user?.id;
-	console.time('getPlans');
-	const plans = await getPlans(userId);
-	console.timeEnd('getPlans');
-	return {
-		plans,
-	};
-}
+export const prerender = true;
