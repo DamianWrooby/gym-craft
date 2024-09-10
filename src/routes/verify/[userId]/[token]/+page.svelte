8e845b21587eaec464ea8160e545f6fa34f1df88
Damian Wróblewski <dwroblewski89@gmail.com>
2024-09-10 20:02:10 +0200

    fix(email-verification): fixed verify page card width

--- src/routes/verify/[userId]/[token]/+page.svelte
+++ src/routes/verify/[userId]/[token]/+page.svelte
@@ -2,8 +2,8 @@
     import Card from '$lib/components/card/Card.svelte';
 </script>
 
-<Card width="1/2 text-center">
-    <div class="md:w-[75%] m-auto">
+<Card>
+    <div class="md:w-[75%] m-auto text-center">
         <p class="pb-5">Your account has been successfully verified. You can now log in.</p>
         <a href="/login" class="btn variant-filled-primary group">
             <span>Log in</span>
