031aef7dc94be529efbcd0281127192179ae75b8
Damian Wróblewski <dwroblewski89@gmail.com>
2024-09-10 20:01:30 +0200

    fix(email-verification): improved email-verification api response

--- src/routes/api/email-verification/[userId]/+server.ts
+++ src/routes/api/email-verification/[userId]/+server.ts
@@ -2,6 +2,8 @@ import { json } from '@sveltejs/kit';
 import { sendVerificationToken } from '$lib/server/mail';
 import { invalidatePreviousToken } from '$lib/prisma/prisma';
 import to from 'await-to-js';
+import { createErrorResponse } from '$lib/utils/error-response';
+import { createResponse } from '$lib/utils/response';
 
 export async function POST({ request, params }: { request: Request; params: { userId: string } }): Promise<Response> {
     const body = await request.json();
@@ -14,7 +16,7 @@ export async function POST({ request, params }: { request: Request; params: { us
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [verificationError, _] = await to(sendVerificationToken(params.userId, email));
-    if (verificationError) return json({ message: 'Verification email not sent' }, { status: 400 });
+    if (verificationError) return createErrorResponse(400, 'Verification email not sent');
 
-    return json({ message: 'Verification email has been sent' });
+    return createResponse(200, 'Verification email has been sent');
 }
