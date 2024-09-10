b052fa150b0414ca47eaf821a144993703907074
Damian Wróblewski <dwroblewski89@gmail.com>
2024-09-10 19:17:36 +0200

    fix(layout): fixed card widths

--- src/routes/verification-mail-sent/+page.svelte
+++ src/routes/verification-mail-sent/+page.svelte
@@ -2,8 +2,8 @@
     import Card from '$lib/components/card/Card.svelte';
 </script>
 
-<Card width="1/2 text-center">
-    <div class="md:w-[75%] m-auto">
+<Card>
+    <div class="md:w-[75%] m-auto text-center">
         <h1 class="h3 pb-4">Thank you for registering with GymCraft!</h1>
         <p>An activation link was sent to the email address you provided.</p>
         <p>Click on the link provided to activate your account, then log in.</p>
