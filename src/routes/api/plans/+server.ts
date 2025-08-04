1177b70910a914ab5c536de55b509f5bfff389b7
Damian Wróblewski <dwroblewski89@gmail.com>
2025-08-04 10:20:15 +0200

    fix(gateway-timeout): fixed timeout + security improvements

--- src/routes/api/plans/+server.ts
+++ src/routes/api/plans/+server.ts
@@ -1,20 +1,19 @@
-import { GeneratedWorkout, NewPlan, Plan, WorkoutSegment, WorkoutStep } from './../../../models/plan/plan.model';
+import { NewPlan, Plan } from './../../../models/plan/plan.model';
 import { addPlan, getGeneralPlanLimit, updateGeneratedPlansNumber, getGeneratedPlansNumber } from '$lib/prisma/prisma';
 import { createResponse } from '$lib/utils/response';
-import { PUBLIC_APP_ENV } from '$env/static/public';
-import { appConfig } from '@/constants/app.constants';
+import { getUserBySession } from '$lib/prisma/prisma';
 import { to } from 'await-to-js';
-import { SurveyFormModel } from './../../../models/survey/survey-form.model';
-import { exerciseMap, workoutCategoriesSet } from '@/constants/workout.constants';
 import type { RequestEvent } from './$types';
 
 export async function POST({ request }: RequestEvent): Promise<Response> {
     const body = await request.json();
-    const userId = body.user.id;
-    const userSession = body.user.session;
-    const { formData }: { formData: SurveyFormModel } = body;
-    const proxyAPIurl = PUBLIC_APP_ENV === 'development' ? appConfig.proxyApiUrlDEV : appConfig.proxyApiUrlPROD;
-    const retryCount = 2;
+    const { session }: { session: string } = body;
+    const { plan }: { plan: Plan } = body;
+
+    // Check user
+    const [userError, user] = await to(getUserBySession(session));
+    if (userError || user.id == null) return createResponse(400, { message: 'User error' });
+    const userId = user.id;
 
     // Check if user has plans left
     const [plansMetaError, results] = await to(Promise.all([getGeneralPlanLimit(), getGeneratedPlansNumber(userId)]));
@@ -25,23 +24,11 @@ export async function POST({ request }: RequestEvent): Promise<Response> {
         return createResponse(400, { message: 'You have reached the limit of generated plans' });
     }
 
-    // Proxy server call
-    const requestBody = JSON.stringify({
-        session: userSession,
-        formData,
-    });
-
-    const validPlan = await tryGenerateValidPlan(retryCount, proxyAPIurl, requestBody);
-
-    if (!validPlan) {
-        return createResponse(502, { message: 'Failed to generate a valid plan after maximum retries' });
-    }
-
     // Add plan to the database
     const newPlan: NewPlan = {
         name: 'Plan ' + generatedPlansNumber,
-        description: validPlan.description,
-        workouts: JSON.parse(JSON.stringify(validPlan.workouts)),
+        description: plan.description,
+        workouts: JSON.parse(JSON.stringify(plan.workouts)),
         User: {
             connect: {
                 id: userId,
@@ -61,112 +48,4 @@ export async function POST({ request }: RequestEvent): Promise<Response> {
     };
 
     return createResponse(200, responseBody);
-}
-
-async function tryGenerateValidPlan(retries: number, url: string, body: string): Promise<Plan | null> {
-    for (let i = 0; i < retries; i++) {
-        const plan = await generatePlan(url, body);
-        if (!plan) {
-            console.log(`Failed to generate plan - retrying (${i + 1}/${retries})`);
-            continue;
-        }
-
-        const improvedPlan = correctPlan(plan);
-        const isValid = isValidPlan(improvedPlan);
-
-        if (isValid) {
-            console.log('Generated plan is valid');
-            return improvedPlan;
-        } else {
-            console.log(`Generated plan is not valid - retrying (${i + 1}/${retries})`);
-            continue;
-        }
-    }
-    return null;
-}
-
-async function generatePlan(url: string, body: string): Promise<Plan | null> {
-    const [proxyError, proxyResponse] = await fetchPlanFromProxyServer(url, body);
-    if (proxyError || !proxyResponse?.ok) return null;
-    const generatedPlan: Plan = await proxyResponse?.json();
-
-    return generatedPlan;
-}
-
-async function fetchPlanFromProxyServer(
-    url: string,
-    body: string,
-): Promise<[null, Response] | [Error | null, undefined]> {
-    return await to(
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: body,
-        }),
-    );
-}
-
-function correctPlan(plan: Plan): Plan {
-    const isPassiveStep = (step: WorkoutStep): boolean => 
-        step.stepType.stepTypeId === 5 || step.stepType.stepTypeId === 4 || step.stepType.stepTypeId === 2;
-
-    const categoryCorrect = (step: WorkoutStep): boolean => {
-        if (!step.category) return false;
-        return workoutCategoriesSet.has(step.category);
-    }
-
-    const exerciseNameCorrect = (step: WorkoutStep): boolean => {
-        if (!step.exerciseName || !step.category) return false;
-        const exercises = exerciseMap.get(step.category);
-        return !!exercises && exercises.has(step.exerciseName);
-    };
-
-    // Ensure if category and exerciseName are null if stepType is cooldown, rest or recovery
-    plan.workouts.forEach((workout: GeneratedWorkout) => {
-        workout.workoutSegments.forEach((segment: WorkoutSegment) => {
-            segment.workoutSteps.forEach((step: WorkoutStep) => {
-                if (isPassiveStep(step) && (step.category || step.exerciseName)) {
-                    console.log('Correcting step:', step);
-                    step.category = null;
-                    step.exerciseName = null;
-                }
-                console.log('Correcting step:', step);
-                if (!isPassiveStep(step) && categoryCorrect(step) && !exerciseNameCorrect(step)) {
-                    step.exerciseName = exerciseMap.get(step.category!)?.values().next().value || null;
-                }
-            });
-        });
-    });
-    return plan;
-}
-
-function isValidPlan(plan: Plan): boolean {
-    return plan.workouts.every((workout: GeneratedWorkout) => {
-        return workout.workoutSegments.every((segment: WorkoutSegment) => {
-            return segment.workoutSteps.every((step: WorkoutStep) => isValidWorkoutStep(step));
-        });
-    });
-}
-
-function isValidWorkoutStep(step: WorkoutStep): boolean {
-    if (!step.stepType || typeof step.stepType.stepTypeId !== 'number') return false;
-
-    // for cooldown, rest and recovery steps omit category and exercise validation
-    if (step.stepType.stepTypeId === 5 || step.stepType.stepTypeId === 4 || step.stepType.stepTypeId === 2) {
-        return true;
-        // for repeat step validate each nested step
-    } else if (step.stepType.stepTypeId === 6) {
-        return (
-            Array.isArray(step.workoutSteps) &&
-            step.workoutSteps?.every((repeatStep: WorkoutStep) => isValidWorkoutStep(repeatStep))
-        );
-    } else {
-        if (!step.category || !step.exerciseName) {
-            console.log('Invalid step:', step);
-            return false;
-        }
-        return !!exerciseMap.get(step.category)?.has(step.exerciseName);
-    }
 }
--- src/routes/app/create-plan/+page.svelte
+++ src/routes/app/create-plan/+page.svelte
@@ -11,6 +11,9 @@
     import type { SurveyFormModel } from '@/models/survey/survey-form.model';
     import type { User } from '@/models/user/user.model';
     import { goto } from '$app/navigation';
+    import type { GeneratedWorkout, Plan, WorkoutSegment, WorkoutStep } from '@models/plan/plan.model';
+    import { exerciseMap, workoutCategoriesSet } from '@/constants/workout.constants';
+    import { PUBLIC_APP_ENV } from '$env/static/public';
 
     const user: User = $page.data.user;
     const { plansLeft: initialPlansLeft } = user;
@@ -21,12 +24,31 @@
     });
 
     const generatePlan = async (event: CustomEvent<{ formData: SurveyFormModel }>) => {
+        const formData = event.detail.formData;
+        const proxyAPIurl = PUBLIC_APP_ENV === 'development' ? appConfig.proxyApiUrlDEV : appConfig.proxyApiUrlPROD;
+        const retryCount = 2;
+
         loadingState.set(true);
 
-        const formData = event.detail.formData;
-        const plansAPIbody = JSON.stringify({
-            user,
+        // PREPARE PROXY SERVER RESPONSE
+        const requestBody = JSON.stringify({
+            session: user.session,
             formData,
+        });
+
+        const validPlan = await tryGenerateValidPlan(retryCount, proxyAPIurl, requestBody);
+
+        if (!validPlan) {
+            makeToast(toastStore, 'Failed to generate a valid plan after maximum retries', 'variant-filled-error');
+            goto('/app');
+            loadingState.set(false);
+            return;
+        }
+
+        // PLANS API
+        const plansAPIbody = JSON.stringify({
+            session: user.session,
+            plan: validPlan,
         });
 
         const [plansAPIError, plansAPIresponse] = await to(
@@ -46,9 +68,9 @@
             return;
         }
         const { generatedPlan, _ } = await plansAPIresponse?.json();
-        
+
         loadingState.set(false);
-        goto(`/app/my-plans/${generatedPlan.id}`)
+        goto(`/app/my-plans/${generatedPlan.id}`);
     };
 
     const planLimitHandler = () => {
@@ -56,6 +78,112 @@
         loadingState.set(false);
         goto('/app');
     };
+
+    async function tryGenerateValidPlan(retries: number, url: string, body: string): Promise<Plan | null> {
+        for (let i = 0; i < retries; i++) {
+            const plan = await fetchPlan(url, body);
+            if (!plan) {
+                // console.log(`Failed to generate plan - retrying (${i + 1}/${retries})`);
+                continue;
+            }
+
+            const improvedPlan = correctPlan(plan);
+            const isValid = isValidPlan(improvedPlan);
+
+            if (isValid) {
+                // console.log('Generated plan is valid');
+                return improvedPlan;
+            } else {
+                // console.log(`Generated plan is not valid - retrying (${i + 1}/${retries})`);
+                continue;
+            }
+        }
+        return null;
+    }
+
+    async function fetchPlan(url: string, body: string): Promise<Plan | null> {
+        const [proxyError, proxyResponse] = await fetchPlanFromProxyServer(url, body);
+        if (proxyError || !proxyResponse?.ok) return null;
+        const generatedPlan: Plan = await proxyResponse?.json();
+
+        return generatedPlan;
+    }
+
+    async function fetchPlanFromProxyServer(
+        url: string,
+        body: string,
+    ): Promise<[null, Response] | [Error | null, undefined]> {
+        return await to(
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: body,
+            }),
+        );
+    }
+
+    function correctPlan(plan: Plan): Plan {
+        const isPassiveStep = (step: WorkoutStep): boolean =>
+            step.stepType.stepTypeId === 5 || step.stepType.stepTypeId === 4 || step.stepType.stepTypeId === 2;
+
+        const categoryCorrect = (step: WorkoutStep): boolean => {
+            if (!step.category) return false;
+            return workoutCategoriesSet.has(step.category);
+        };
+
+        const exerciseNameCorrect = (step: WorkoutStep): boolean => {
+            if (!step.exerciseName || !step.category) return false;
+            const exercises = exerciseMap.get(step.category);
+            return !!exercises && exercises.has(step.exerciseName);
+        };
+
+        // Ensure if category and exerciseName are null if stepType is cooldown, rest or recovery
+        plan.workouts.forEach((workout: GeneratedWorkout) => {
+            workout.workoutSegments.forEach((segment: WorkoutSegment) => {
+                segment.workoutSteps.forEach((step: WorkoutStep) => {
+                    if (isPassiveStep(step) && (step.category || step.exerciseName)) {
+                        step.category = null;
+                        step.exerciseName = null;
+                    }
+                    if (!isPassiveStep(step) && categoryCorrect(step) && !exerciseNameCorrect(step)) {
+                        step.exerciseName = exerciseMap.get(step.category!)?.values().next().value || null;
+                    }
+                });
+            });
+        });
+        return plan;
+    }
+
+    function isValidPlan(plan: Plan): boolean {
+        return plan.workouts.every((workout: GeneratedWorkout) => {
+            return workout.workoutSegments.every((segment: WorkoutSegment) => {
+                return segment.workoutSteps.every((step: WorkoutStep) => isValidWorkoutStep(step));
+            });
+        });
+    }
+
+    function isValidWorkoutStep(step: WorkoutStep): boolean {
+        if (!step.stepType || typeof step.stepType.stepTypeId !== 'number') return false;
+
+        // for cooldown, rest and recovery steps omit category and exercise validation
+        if (step.stepType.stepTypeId === 5 || step.stepType.stepTypeId === 4 || step.stepType.stepTypeId === 2) {
+            return true;
+            // for repeat step validate each nested step
+        } else if (step.stepType.stepTypeId === 6) {
+            return (
+                Array.isArray(step.workoutSteps) &&
+                step.workoutSteps?.every((repeatStep: WorkoutStep) => isValidWorkoutStep(repeatStep))
+            );
+        } else {
+            if (!step.category || !step.exerciseName) {
+                // console.log('Invalid step:', step);
+                return false;
+            }
+            return !!exerciseMap.get(step.category)?.has(step.exerciseName);
+        }
+    }
 </script>
 
 {#if $loadingState}
